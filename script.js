4a339f45dfa1ee624fabfe22bda306c290d53d12
aravindsomabathina <aravindsomabathina22@gmail.com>
2025-03-27 22:48:41 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -7,3 +7,16 @@ var vinay = [...aravind];
 vinay.pop();
 console.log(aravind);
 console.log(vinay);
+for(var a=3;a>=1;a--){
+    console.log(a);
+
+}
+function simple(val){
+    var i=12;
+    console.log(i+val);
+    /*for(var b=12;b<22;b++)
+    {
+        console.log("hello");
+    }*/
+}
+simple(12);
