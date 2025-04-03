369bb6874f75a35bbdc2a7dc47c68f46d25ab40e
aravindsomabathina <aravindsomabathina22@gmail.com>
2025-04-03 18:14:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -20,3 +20,53 @@ function simple(val){
     }*/
 }
 simple(12);
+//String Slice
+//------------------------------
+let str="apple";
+let ans=str.slice(0,4);
+console.log(ans);
+
+
+
+//String Template literals(Backticks)
+//------------------------------
+let a=`hello i am aravind${1+2}`;
+console.log(a);
+
+
+//String Split
+//------------------------------
+let b="hello world";
+let answ=b.split('d');
+console.log(answ)
+
+//String Replace
+//------------------------------
+let string="hello world";
+let answer=string.replace('w','y');
+let answer1=string.replaceAll('w','an');
+console.log(answer);
+console.log(answer1);
+
+
+//String Includes
+//------------------------------
+let strin="hello world!";
+let sol=str.includes(" ");
+console.log(sol);
+
+//SWITCH
+//------------------------------
+switch(3){
+    case 1:console.log("one");
+            break;
+    case 2:console.log("two");
+            break;
+    case 3:console.log("three");
+            break;
+    case 4:console.log("four");
+            break;
+    default:console.log("default")
+
+}
+
