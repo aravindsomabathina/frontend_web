49e20291607bbd89402de1e345f5fd1b8f4b6abb
aravindsomabathina <aravindsomabathina22@gmail.com>
2025-04-16 16:54:41 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,139 +1,47 @@
-/*var a=12;
-var b=a;
-console.log(a);
-console.log(b+2);
-var aravind=[1,2,3];
-var vinay = [...aravind];
-vinay.pop();
-console.log(aravind);
-console.log(vinay);
-for(var a=3;a>=1;a--){
-    console.log(a);
-
-}
-function simple(val){
-    var i=12;
-    console.log(i+val);
-    /*for(var b=12;b<22;b++)
-    {
-        console.log("hello");
-    }*/
-/*var arr=[1,2,"3",function(){},[],4];
-console.log(arr[2]);
-var obj={
-    name:"Aravind",
-    age:22,
-    mail:SpeechR,
-
-};
-console.log(obj.name);*/
-/*let rectangle={
-    length:20,
-    width:10,
-    area:function(){
-        console.log(this.length*this.width);
-        
-    }
-}*/
-//String Slice
-//------------------------------
-//let str="apple";
-//let ans=str.slice(0,4);
-//console.log(ans);
+//****************************  MAP  *****************************************
+var arr = [1,2,3,4];
+var ans = arr.map(function(value){
+    return 12;
+})
+console.log(ans);
 
 
 
-//String Template literals(Backticks)
-//------------------------------
-//let a=`hello i am aravind${1+2}`;
-////console.log(a);
+//*****************************  FILTER **************************************
+
+var arr = [1,2,3,4];
+var ans = arr.filter(function(value){
+    return value>2;
+})
+console.log(ans);
 
 
-//String Split
-//------------------------------
-//let b="hello world";
-//let answ=b.split('d');
-//console.log(answ)
+//*****************************  REDUCE   ***********************************
 
-//String Replace
-//------------------------------
-/*let string="hello world";
-let answer=string.replace('w','y');
-let answer1=string.replaceAll('w','an');
-console.log(answer);
-console.log(answer1);
+var arr = [1,2,3,4];
+var ans = arr.reduce(function(accumulator, key){
+    return accumulator+key;
+}, 0);
+console.log(ans);
 
 
-//String Includes
-//------------------------------
-let strin="hello world!";
-let sol=str.includes(" ");
-console.log(sol);
+//Remove duplicate elements from the array
+//-----------------------------------------------------------------------------------
 
-//SWITCH
-//------------------------------
-/*switch(3){
-    case 1:console.log("one");
-            break;
-    case 2:console.log("two");
-            break;
-    case 3:console.log("three");
-            break;
-    case 4:console.log("four");
-            break;
-    default:console.log("default")
-
-}*/
-// IIFE(Immediatley invoked function Expression)
-//----------------------------------------------------
-var flm=function flimlibrary(){
-    return{
-        imageEffect:function(){
-            console.log("hello world");
-
-        },
-        mousefollow:function(){
-            console.log("hello india");
-        }
-    }
-}();
-flm.mousefollow();
-//----------------------------------------------------
-function abcd(){
-    console.log("Aravind");
-}
-
-abcd();
-//---------------------------------------------------
-//Higher order function
-function aravind(){
-    return function(){
-        return function(){
-            console.log("inside function");
-        }
-    }
-
-}
-
-aravind()()();
-//-------------------------------------------------------
-//Global Scope, Local Scope
-function vinay(){
-    let a=10;
-    console.log(a);
-}
-vinay();
-
-//---------------------------------------------------------
-//first order function
-function multiply(a,b){
-    return(a*b);
-
-}
-let result=multiply(4,3);
-console.log(result);
+var arr=[1,1,2,2,3,3,4,5,6,7];
+var ans=[...new Set(arr)];
+console.log(ans);
 
 
+//second largest element in an array
+//---------------------------------------------------------------------------------------------
 
 
+var arr=[1,1,2,2,3,3,4,5,6,7];
+var ans=[...new Set(arr)];
+var newarr= ans.sort(function(a,b){
+    return b-a;
+
+});
+console.log(newarr[1]);
 
